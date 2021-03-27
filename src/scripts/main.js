240a5466717267f9515a27356c4810c578f66f38
rutger <r.touburg@beeldsluiter.nl>
2021-03-27 17:03:31 +0100

    first commit, for review so far, system darkmode partially implemented button non functioning yet added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+const togbtn = document.querySelector('.btn-toggle');
+
 $(window).load(function() {
     $('.sidenav').sidenav();
     $('.language-button-small').dropdown();
@@ -6,4 +8,10 @@ $(window).load(function() {
     $('.flexslider').flexslider({
         animation: 'fade'
     });
+});
+
+// Listen for a click on the button
+togbtn.addEventListener('click', function() {
+    // Then toggle (add/remove) the .dark-theme class to the body
+    document.body.classList.toggle('dark-theme');  
 });
